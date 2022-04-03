4e639a518619ea5d674d5fd5d46b0c23b278d514
Mert <pme2012@yandex.com>
2022-04-04 02:03:57 +0300

    gracefull2

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/pMertDogan/picusGoBackend--Patika/picusBootCampFinalProject/database"
 	"github.com/pMertDogan/picusGoBackend--Patika/picusBootCampFinalProject/domain/category"
 	"github.com/pMertDogan/picusGoBackend--Patika/picusBootCampFinalProject/domain/check"
 	"github.com/pMertDogan/picusGoBackend--Patika/picusBootCampFinalProject/pkg/config"
+	"github.com/pMertDogan/picusGoBackend--Patika/picusBootCampFinalProject/pkg/graceful"
 	logger "github.com/pMertDogan/picusGoBackend--Patika/picusBootCampFinalProject/pkg/logging"
 	"go.uber.org/zap"
 )
@@ -40,5 +44,27 @@ func main() {
 
 	category.CategoryControllerDef(router)
 	check.CheckControllerDef(router)
-	router.Run(":8080")
+	// router.Run(":8080")
+
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%s", cfg.ServerConfig.Port),
+		Handler:      router,
+		ReadTimeout:  time.Duration(cfg.ServerConfig.ReadTimeoutSecs * int64(time.Second)),
+		WriteTimeout: time.Duration(cfg.ServerConfig.WriteTimeoutSecs * int64(time.Second)),
+	}
+
+	//Graceful Shutdown & Restart
+	//https://grisha.org/blog/2014/06/03/graceful-restart-in-golang/
+	//https://gin-gonic.com/docs/examples/graceful-restart-or-stop/
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	log.Println("Book store service started")
+	graceful.ShutdownGin(srv, time.Duration(cfg.ServerConfig.TimeoutSecs*int64(time.Second)))
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,10 +8,22 @@ import (
 )
 
 type Config struct {
-	// ServerConfig ServerConfig
+	ServerConfig ServerConfig
 	// JWTConfig    JWTConfig
 	DBConfig DBConfig
 	Logger   Logger
+}
+
+
+type ServerConfig struct {
+	AppVersion       string
+	Mode             string
+	RoutePrefix      string
+	Debug            bool
+	Port             string
+	TimeoutSecs      int64
+	ReadTimeoutSecs  int64
+	WriteTimeoutSecs int64
 }
 
 /*
